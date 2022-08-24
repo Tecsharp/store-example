86ab2e6aed9ceb92e771b57eb7ef34ff5d8afdb0
tecsharp <a.tecsharp@gmail.com>
2022-08-23 22:01:12 -0500

    Prueba de commit desde eclipse

--- src/main/java/com/tecsharp/store/repository/users/UsuariosRpository.java
+++ src/main/java/com/tecsharp/store/repository/users/UsuariosRpository.java
@@ -1,5 +1,11 @@
 package com.tecsharp.store.repository.users;
 
+import java.util.List;
+
+import com.tecsharp.store.entity.usuarios.Usuario;
+
 public interface UsuariosRpository {
 
+	List<Usuario> obtenerUsuarios();
+	
 }
